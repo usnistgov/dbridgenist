1831b9d3df771d7549fd0f05b28ddca79ee62928
Jordan Love <jordan.love@nist.gov>
2023-06-30 15:34:33 -0400

    change data collection method

--- python/test_speed.py
+++ python/test_speed.py
@@ -13,15 +13,18 @@ ADC = ADS1263.ADS1263()
 if (ADC.ADS1263_init_ADC1('ADS1263_38400SPS') == -1):
     exit()
 ADC.ADS1263_SetMode(0)
+ADC.SetChannal(0)
 
 times = []
 n = input("n: ")
 
 for i in range(int(n)):
     start_time = time.time()
-
+    
     ADC.ADS1263_WaitDRDY()
+    Value = ADC.ADS1263_Read_ADC_Data()
 
     times.append(time.time()-start_time)
     
+print(times)
 print(sum(times)/len(times))

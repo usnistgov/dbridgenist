85cb2272ad5931736633bddb5e3b61db7aba0aa3
Jordan Love <jordan.love@nist.gov>
2023-06-30 16:17:45 -0400

    update data collection method

--- python/test_speed.py
+++ python/test_speed.py
@@ -17,20 +17,27 @@ ADC.ADS1263_SetMode(0)
 ADC.ADS1263_SetChannal(0)
 
 times = []
+values = []
 n = input("n: ")
 
 for i in range(int(n)):
     start_time = time.time()
     
-    #ADC.ADS1263_WaitDRDY()
-    #Value = ADC.ADS1263_Read_ADC_Data()
+    ADC.ADS1263_WaitDRDY()
 
+    config.digital_write(22, GPIO.LOW)
     while(1):
-        config.spi_writebyte(0x12)
+        config.spi_writebyte([0x12])
         if(config.spi_readbytes(1)[0] & 0x40 != 0):
             break
+    buf = config.spi_readbytes(5)
+    config.digital_write(22, GPIO.HIGH)
+
+    read = (buf[0]<<16) & 0xff000000 | (buf[1]<<8) & 0xff0000 | (buf[2]) & 0xff00 | (buf[3]) & 0xff
+    values.append(read)
 
     times.append(time.time()-start_time)
     
-print(times)
+print(values)
 print(sum(times)/len(times))
+print(len(times)/sum(times))

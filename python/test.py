2ad52495742e2c510bac86f22fac89f68d4befe6
Jordan Love <jordan.love@nist.gov>
2023-07-10 11:12:57 -0400

    fix errors

--- python/test.py
+++ python/test.py
@@ -1,5 +1,6 @@
 import time
 import ADS1263
+import config
 import RPi.GPIO as GPIO
 ADC = ADS1263.ADS1263()
 if ADC.ADS1263_init_ADC1('ADS1263_38400SPS') == -1:
@@ -9,16 +10,18 @@ ADC.ADS1263_SetChannal(0)
 
 ADC.ADS1263_WaitDRDY()
 times = []
+cs_pin = 22
+
 now = time.time()
 for i in range(1000):
-    config.digital_write(self.cs_pin, GPIO.LOW)
-	config.spi_writebyte([ADS1263_CMD['CMD_RDATA1']])
+    config.digital_write(cs_pin, GPIO.LOW)
+    config.spi_writebyte([0x12])
     buf = config.spi_readbytes(5)
-    config.digital_write(self.cs_pin, GPIO.HIGH)
-    read  = (buf[0]<<24) & 0xff000000 | (buf[1]<<16) & 0xff0000 | (buf[2]<<8) & 0xff00 | read |= (buf[3]) & 0xff
-	later = time.time()
-	times.append(later-now)
-	now = later
+    config.digital_write(cs_pin, GPIO.HIGH)
+    read  = (buf[0]<<24) & 0xff000000 | (buf[1]<<16) & 0xff0000 | (buf[2]<<8) & 0xff00 | (buf[3]) & 0xff
+    later = time.time()
+    times.append(later-now)
+    now = later
     time.sleep(0.001)
 
 with open('test.dat', 'a') as f:

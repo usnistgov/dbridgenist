86a8e4cfbcba0a1a4c4ebd6daea8b537bae6664e
Jordan Love <jordan.love@nist.gov>
2023-07-14 15:38:16 -0400

    fix errors

--- python/serial_pc.py
+++ python/serial_pc.py
@@ -11,23 +11,24 @@ import sys
 
 ser = serial.Serial('COM5', 19200, timeout=1)
 
-def int2float(meas,REF):
-    if meas >> 31 == 1: 
-        ret = - meas / 0x80000000
-    elif meas >> 31 != 1:
-        ret =  meas / 0x7fffffff
+def int2float(num):
+    if num >> 31 == 1: 
+        ret = -num / 0x80000000
+    elif num >> 31 != 1:
+        ret =  num / 0x7fffffff
     return ret
 
 try:
     while True:
         try:
-            ret = ser.read(2)
+            ret = ser.read(11)
             if ret != b'':
                 ret = int(ret.decode())
                 print(int2float(ret))
         except KeyboardInterrupt:
             break
-except:
+except Exception as e:
+    #print(e)
     ser.close()
     
 ser.close()

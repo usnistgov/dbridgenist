9537c049ba4ed756055ea0a789d69bdafca5b8f1
Jordan Love <jordan.love@nist.gov>
2023-07-14 12:42:28 -0400

    add timeout

--- python/serial_pc.py
+++ python/serial_pc.py
@@ -8,8 +8,8 @@ Created on Fri Jul 14 10:38:49 2023
 import serial
 import time
 
-ser = serial.Serial('COM5')
+ser = serial.Serial('COM5', 30000, timeout=1)
 
-ser.read(5)
+ser.read(2)
 
 ser.close()

533e569f4a3559acb41b9c649bf9889e4900b101
Jordan Love <jordan.love@nist.gov>
2023-07-14 11:54:39 -0400

    change get_data

--- python/serial_rpi.py
+++ python/serial_rpi.py
@@ -7,12 +7,18 @@ Created on Fri Jul 14 10:17:05 2023
 
 import serial
 import threading
+import time
 
 def get_data():
     ser = serial.Serial('/dev/ttyAMA0')
-    print(ser.name)
-    ser.write(b'hello')
+    
+    for i in range(30):
+        ret = 30-i
+        ret = ret.to_bytes(2, 'big')
+        ser.write(ret)
+        time.sleep(1)
+    
     ser.close()
     
-timer = threading.Timer(30.0, get_data)
+timer = threading.Timer(5.0, get_data)
 timer.start()

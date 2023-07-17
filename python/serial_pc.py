e4cdafac67f6861a633639129ee6e8997e10cf63
Jordan Love <jordan.love@nist.gov>
2023-07-17 15:17:28 -0400

    constantly transmist data from rpi

--- python/serial_pc.py
+++ python/serial_pc.py
@@ -26,22 +26,35 @@ def int2float2(num, REF):
         ret =  num * REF / 0x7fffffff
     return round(ret, 6)
 
+ser = serial.Serial('COM5', 19200, timeout=1)
 try:
+    REF = 4.76
+    ser.write(b'pcready')
+    conf = ser.read(7)
+    i = 0
+    while conf != b'piready':
+        conf = ser.read(7)
+        i += 1
+        if i >= 30:
+            raise TimeoutError('Could not communicate with pi')
     while True:
         try:
             length = ser.read(2)
-            ret = ser.read(length)
-            print(ret)
+            ret = ser.read(int(length.decode()))
             if ret != b'':
                 ret = ret.decode()
                 ret = ret.split()
                 with open("test.dat", 'a') as f:
-                    f.write('{0:.6}{1:10.6f}\n'.format(int2float(int(ret[0])),  int2float2(int(ret[1]), REF)))
+                    s = '{0:.6} {1:10.6f}\n'.format(int2float(int(ret[0])),  int2float2(int(ret[1]), REF))
+                    f.write(s)
+                    print(s)
         except KeyboardInterrupt:
             break
 
 except Exception as e:
     print(e)
+    ser.write(b'done')
     ser.close()
-    
+
+ser.write(b'done')
 ser.close()
--- python/serial_rpi.py
+++ python/serial_rpi.py
@@ -8,6 +8,13 @@ Created on Fri Jul 14 10:17:05 2023
 import serial
 import threading
 import time
+import ADS1263
+
+dt = 0.001
+data = []
+ts = []
+done = False
+t0 = time.time()
 
 def float2int(num):
     if num>=0:
@@ -16,23 +23,44 @@ def float2int(num):
         ret = int(-num*0x80000000)
     return ret
 
-def encode(num, nbytes):
+def encode(num):
     arr = bytearray()
-    num = "{0:0{1}}".format(num, nbytes)
+    num = "{0}".format(num)
     for x in num:
         arr.append(int(ord(x)))
     return arr
 
-def get_data():
-    ser = serial.Serial('/dev/ttyAMA0', 19200, timeout=1)
+def reset():
+    global data, ts, done, t0
+    data = []
+    ts = []
+    done = False
+    t0 = time.time()
+
+def getData():
+    global data, ts, done, ADC, t0
+    if not done:
+        t1 = threading.Timer(dt,getData)
+        t1.start()
+    now = time.time()
+    rt = now - t0
+    t1 = time.time()
+    meas = ADC.ADS1263_Read_ADC_Data()
+    t2 = time.time()
+    rt = 0.5*(t1+t2)-t0
+    data.append(meas)
+    ts.append(rt)
+
+ser = serial.Serial('/dev/ttyAMA0', 19200, timeout=1)    
+while True:
+    conf = ser.read(7)
+    while conf != b'pcready':
+        conf = ser.read(7)
+    ser.write(b'piready')
+    reset()
+    t1 = threading.Timer(dt,getData)
+    conf = ser.read(4)
+    while conf != b'done':
+        conf = ser.read(4)
+    done = True
     
-    for i in range(300):
-        ret = float2int((300-i)/10)
-        ret = encode(ret, 11)
-        ser.write(ret)
-        time.sleep(0.1)
-    
-    ser.close()
-    
-timer = threading.Timer(5.0, get_data)
-timer.start()

c6d7e5b57b3deaa7b12b98797f3a36befb65052e
Jordan Love <jordan.love@nist.gov>
2023-07-18 14:30:30 -0400

    change dt

--- python/serial_rpi.py
+++ python/serial_rpi.py
@@ -10,7 +10,7 @@ import threading
 import time
 import ADS1263
 
-dt = 0.002
+dt = 0.0005
 N = 10
 ADC = ADS1263.ADS1263()
 if (ADC.ADS1263_init_ADC1('ADS1263_38400SPS') == -1):
@@ -49,7 +49,7 @@ def reset():
     data = []
     ts = []
     done = False
-    dt = 0.002
+    dt = 0.0005
 
 def addData():
     global ser

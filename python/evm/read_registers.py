82ee42e5a5b35bd0a62ff2490b7f0a415df50c1a
Jordan Love <jordan.love@nist.gov>
2023-08-01 13:02:50 -0400

    add chip select toggle

--- python/evm/read_registers.py
+++ python/evm/read_registers.py
@@ -7,11 +7,15 @@ spi = spidev.SpiDev()
 spi.open(0, 1)
 spi.max_speed_hz = 2000000
 spi.mode = 0b01
+CS = 16
+GPIO.setup(CS, GPIO.OUT)
 
 result = spi.xfer([0x06])
 for reg in range(27):
+	GPIO.output(CS, GPIO.LOW)
 	spi.writebytes([0x20 | reg, 0x00])
 	result2 = spi.readbytes(1)
 	print(format(reg, '#04x'), ':', format(result2[0], '#04x'),':', format(result2[0], '#010b'))
+	GPIO.output(CS, GPIO.HIGH)
 
 spi.close()
